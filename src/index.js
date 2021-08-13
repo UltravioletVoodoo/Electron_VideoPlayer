9fe7d2202f0528d389935ec5d7785a11496c7f7d
Jonathan <jonbezeau@gmail.com>
2021-08-13 15:47:33 -0700

    Implemented basic skeleton

---
+++ src/index.js
@@ -0,0 +1,4 @@
+function toggleLibrary() {
+    document.getElementById('libraryModal').classList.toggle('libraryModal-open');
+    document.getElementById('libraryModal-toggleButton').classList.toggle('flip');
+}
